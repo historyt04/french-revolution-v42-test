5c17bf6bf41cc107bc4d75b89160b7d1b057f132
historyt04 <historyt04@gmail.com>
2026-09-19 16:38:08 +0900

    Show Dongju Middle School on student login
    Updated HISTORY_API_CONFIG for deployment configuration.

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-/* No secret belongs here. localhost automatically uses the local test server. */
-window.HISTORY_API_CONFIG={mode:'gas',gasUrl:'https://script.google.com/macros/s/AKfycbyuQA-jsKRzdY7mTnAoZn9XbUWugRlMfLC6wc6iWizD9i2gAwjeERZ9HcbX5SCPj9mk/exec'};
+/* Configure only this file for deployment. mode: 'gas' or 'local'. */
+window.HISTORY_API_CONFIG={mode:'gas',gasUrl:'https://script.google.com/macros/s/AKfycbyuQA-jsKRzdY7mTnAoZn9XbUWugRlMfLC6wc6iWizD9i2gAwjeERZ9HcbX5SCPj9mk/exec',schoolLabels:{'school-01':'동주중학교'}};
